14dd724634381fd456b6260ce58126f1dacce04c
Vitor Marinho <vitormarinhoh@gmail.com>
2024-06-07 16:06:20 -0300

    Adicionado a logo

--- src/PacienteMenu.jsx
+++ src/PacienteMenu.jsx
@@ -72,26 +72,7 @@ const PacienteMenu = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
+            <Link to="/cadastro-produtos" className="button">Cadastrar Produtos</Link>
             </ul>
             <form className="d-flex" role="search">
             </form>
